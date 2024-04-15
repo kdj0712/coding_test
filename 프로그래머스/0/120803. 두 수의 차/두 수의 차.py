70adfc80d6bb063c78195daea0f72107b7d61d12
DeokJae Kim <151099214+kdj0712@users.noreply.github.com>
2024-04-15 19:16:31 +0900

    [level 0] Title: 두 수의 차, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.py
@@ -0,0 +1,12 @@
+def solution(num1, num2):
+    answer = num1 - num2
+    return answer
+
+def solve():
+    num1 = 2
+    num2 = 3
+    answer = solution(num1, num2)
+    print("{}".format(answer))
+    return
+
+solve()
