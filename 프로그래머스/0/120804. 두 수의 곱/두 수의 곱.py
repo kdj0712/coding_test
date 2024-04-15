f6ee650de9f639be6f075586935c7a3c00326f15
DeokJae Kim <151099214+kdj0712@users.noreply.github.com>
2024-04-15 19:16:40 +0900

    [level 0] Title: 두 수의 곱, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.py
@@ -0,0 +1,12 @@
+def solution(num1, num2):
+    answer = num1 * num2
+    return answer
+
+def solve():
+    num1 = 27
+    num2 = 19
+    answer = solution(num1, num2)
+    print("{}".format(answer))
+    return
+
+solve()
