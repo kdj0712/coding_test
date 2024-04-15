10e8bd01cae279620cacacadd3da8bea2b71c742
DeokJae Kim <151099214+kdj0712@users.noreply.github.com>
2024-04-15 19:16:50 +0900

    [level 0] Title: 몫 구하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.py
@@ -0,0 +1,12 @@
+def solution(num1, num2):
+    answer = num1 / num2
+    return int(answer)
+
+def solve():
+    num1 = 7
+    num2 = 2
+    answer = solution(num1, num2)
+    print("{}".format(answer))
+    return
+
+solve()
