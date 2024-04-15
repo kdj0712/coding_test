8dc11ed48de3d5584d9974f175d4cc9df85ed80d
DeokJae Kim <151099214+kdj0712@users.noreply.github.com>
2024-04-15 19:25:23 +0900

    [level 0] Title: 편지, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.py
@@ -0,0 +1,12 @@
+def solution(message):
+    answer = 0
+    answer = len(message) * 2
+    return answer
+
+def solve():
+    message = "I love you~"
+    answer = solution(message)
+    print("{}".format(answer))
+    return
+
+solve()
